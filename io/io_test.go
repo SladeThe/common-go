9aa63c270e2dea19c555ea8e28fbaeaa909e4b03
SladeThe <sladethe@gmail.com>
2019-11-17 14:06:12 +0400

    Fix panic in tests

--- io/io_test.go
+++ io/io_test.go
@@ -50,14 +50,18 @@ func TestDiffDirsB_DifferentContent(t *testing.T) {
 	assert.Equal(t, 1, len(diffs))
 
 	diff := diffs[0]
-	assert.Equal(t, "0.bin", diff.Item1.Name())
-	assert.Equal(t, "0.bin", diff.Item2.Name())
-	assert.Equal(t, diffPath("b1/diff/diff/0.bin"), diff.Item1.FullPath)
-	assert.Equal(t, diffPath("b2/diff/diff/0.bin"), diff.Item2.FullPath)
-	assert.True(t, diff.Item1.IsFile())
-	assert.True(t, diff.Item2.IsFile())
-	assert.False(t, diff.Item1.IsDir())
-	assert.False(t, diff.Item2.IsDir())
+	if assert.NotNil(t, diff.Item1) {
+		assert.Equal(t, "0.bin", diff.Item1.Name())
+		assert.Equal(t, diffPath("b1/diff/diff/0.bin"), diff.Item1.FullPath)
+		assert.True(t, diff.Item1.IsFile())
+		assert.False(t, diff.Item1.IsDir())
+	}
+	if assert.NotNil(t, diff.Item2) {
+		assert.Equal(t, "0.bin", diff.Item2.Name())
+		assert.Equal(t, diffPath("b2/diff/diff/0.bin"), diff.Item2.FullPath)
+		assert.True(t, diff.Item2.IsFile())
+		assert.False(t, diff.Item2.IsDir())
+	}
 }
 
 func TestDiffDirsC_DifferentContentStructure(t *testing.T) {
@@ -67,115 +71,151 @@ func TestDiffDirsC_DifferentContentStructure(t *testing.T) {
 
 	{
 		diff := diffs[0]
-		assert.Equal(t, diffPath("c1/s0"), diff.Item1.FullPath)
+		if assert.NotNil(t, diff.Item1) {
+			assert.Equal(t, diffPath("c1/s0"), diff.Item1.FullPath)
+			assert.False(t, diff.Item1.IsFile())
+			assert.True(t, diff.Item1.IsDir())
+		}
 		assert.Nil(t, diff.Item2)
-		assert.False(t, diff.Item1.IsFile())
-		assert.True(t, diff.Item1.IsDir())
 	}
 
 	{
 		diff := diffs[1]
 		assert.Nil(t, diff.Item1)
-		assert.Equal(t, diffPath("c2/s1/S2"), diff.Item2.FullPath)
-		assert.False(t, diff.Item2.IsFile())
-		assert.True(t, diff.Item2.IsDir())
+		if assert.NotNil(t, diff.Item2) {
+			assert.Equal(t, diffPath("c2/s1/S2"), diff.Item2.FullPath)
+			assert.False(t, diff.Item2.IsFile())
+			assert.True(t, diff.Item2.IsDir())
+		}
 	}
 
 	{
 		diff := diffs[2]
-		assert.Equal(t, "0.bin", diff.Item1.Name())
-		assert.Equal(t, "0.bin", diff.Item2.Name())
-		assert.Equal(t, diffPath("c1/s1/s1/0.bin"), diff.Item1.FullPath)
-		assert.Equal(t, diffPath("c2/s1/s1/0.bin"), diff.Item2.FullPath)
-		assert.True(t, diff.Item1.IsFile())
-		assert.True(t, diff.Item2.IsFile())
-		assert.False(t, diff.Item1.IsDir())
-		assert.False(t, diff.Item2.IsDir())
+		if assert.NotNil(t, diff.Item1) {
+			assert.Equal(t, "0.bin", diff.Item1.Name())
+			assert.Equal(t, diffPath("c1/s1/s1/0.bin"), diff.Item1.FullPath)
+			assert.True(t, diff.Item1.IsFile())
+			assert.False(t, diff.Item1.IsDir())
+		}
+		if assert.NotNil(t, diff.Item2) {
+			assert.Equal(t, "0.bin", diff.Item2.Name())
+			assert.Equal(t, diffPath("c2/s1/s1/0.bin"), diff.Item2.FullPath)
+			assert.True(t, diff.Item2.IsFile())
+			assert.False(t, diff.Item2.IsDir())
+		}
 	}
 
 	{
 		diff := diffs[3]
-		assert.Equal(t, diffPath("c1/s1/s2"), diff.Item1.FullPath)
+		if assert.NotNil(t, diff.Item1) {
+			assert.Equal(t, diffPath("c1/s1/s2"), diff.Item1.FullPath)
+			assert.False(t, diff.Item1.IsFile())
+			assert.True(t, diff.Item1.IsDir())
+		}
 		assert.Nil(t, diff.Item2)
-		assert.False(t, diff.Item1.IsFile())
-		assert.True(t, diff.Item1.IsDir())
 	}
 
 	{
 		diff := diffs[4]
 		assert.Nil(t, diff.Item1)
-		assert.Equal(t, diffPath("c2/s2"), diff.Item2.FullPath)
-		assert.False(t, diff.Item2.IsFile())
-		assert.True(t, diff.Item2.IsDir())
+		if assert.NotNil(t, diff.Item2) {
+			assert.Equal(t, diffPath("c2/s2"), diff.Item2.FullPath)
+			assert.False(t, diff.Item2.IsFile())
+			assert.True(t, diff.Item2.IsDir())
+		}
 	}
 
 	{
 		diff := diffs[5]
-		assert.Equal(t, diffPath("c1/s3"), diff.Item1.FullPath)
+		if assert.NotNil(t, diff.Item1) {
+			assert.Equal(t, diffPath("c1/s3"), diff.Item1.FullPath)
+			assert.False(t, diff.Item1.IsFile())
+			assert.True(t, diff.Item1.IsDir())
+		}
 		assert.Nil(t, diff.Item2)
-		assert.False(t, diff.Item1.IsFile())
-		assert.True(t, diff.Item1.IsDir())
 	}
 
 	{
 		diff := diffs[6]
 		assert.Nil(t, diff.Item1)
-		assert.Equal(t, diffPath("c2/s4"), diff.Item2.FullPath)
-		assert.False(t, diff.Item2.IsFile())
-		assert.True(t, diff.Item2.IsDir())
+		if assert.NotNil(t, diff.Item2) {
+			assert.Equal(t, diffPath("c2/s4"), diff.Item2.FullPath)
+			assert.False(t, diff.Item2.IsFile())
+			assert.True(t, diff.Item2.IsDir())
+		}
 	}
 
 	{
 		diff := diffs[7]
 		assert.Nil(t, diff.Item1)
-		assert.Equal(t, diffPath("c2/s5/aa"), diff.Item2.FullPath)
+		if assert.NotNil(t, diff.Item2) {
+			assert.Equal(t, diffPath("c2/s5/aa"), diff.Item2.FullPath)
+		}
 	}
 
 	{
 		diff := diffs[8]
-		assert.Equal(t, diffPath("c1/s5/s0/0.bin"), diff.Item1.FullPath)
+		if assert.NotNil(t, diff.Item1) {
+			assert.Equal(t, diffPath("c1/s5/s0/0.bin"), diff.Item1.FullPath)
+		}
 		assert.Nil(t, diff.Item2)
 	}
 
 	{
 		diff := diffs[9]
 		assert.Nil(t, diff.Item1)
-		assert.Equal(t, diffPath("c2/s5/s0/1.bin"), diff.Item2.FullPath)
+		if assert.NotNil(t, diff.Item2) {
+			assert.Equal(t, diffPath("c2/s5/s0/1.bin"), diff.Item2.FullPath)
+		}
 	}
 
 	{
 		diff := diffs[10]
-		assert.Equal(t, diffPath("c1/s5/s2/0.bin"), diff.Item1.FullPath)
-		assert.Equal(t, diffPath("c2/s5/s2/0.bin"), diff.Item2.FullPath)
-		assert.True(t, diff.Item1.IsFile())
-		assert.True(t, diff.Item2.IsFile())
+		if assert.NotNil(t, diff.Item1) {
+			assert.Equal(t, diffPath("c1/s5/s2/0.bin"), diff.Item1.FullPath)
+			assert.True(t, diff.Item1.IsFile())
+		}
+		if assert.NotNil(t, diff.Item2) {
+			assert.Equal(t, diffPath("c2/s5/s2/0.bin"), diff.Item2.FullPath)
+			assert.True(t, diff.Item2.IsFile())
+		}
 	}
 
 	{
 		diff := diffs[11]
 		assert.Nil(t, diff.Item1)
-		assert.Equal(t, diffPath("c2/s5/s2/1.bin"), diff.Item2.FullPath)
-		assert.True(t, diff.Item2.IsFile())
+		if assert.NotNil(t, diff.Item2) {
+			assert.Equal(t, diffPath("c2/s5/s2/1.bin"), diff.Item2.FullPath)
+			assert.True(t, diff.Item2.IsFile())
+		}
 	}
 
 	{
 		diff := diffs[12]
-		assert.Equal(t, diffPath("c1/s5/s3/0.bin"), diff.Item1.FullPath)
-		assert.Equal(t, diffPath("c2/s5/s3/0.bin"), diff.Item2.FullPath)
-		assert.True(t, diff.Item1.IsFile())
-		assert.True(t, diff.Item2.IsDir())
+		if assert.NotNil(t, diff.Item1) {
+			assert.Equal(t, diffPath("c1/s5/s3/0.bin"), diff.Item1.FullPath)
+			assert.True(t, diff.Item1.IsFile())
+		}
+		if assert.NotNil(t, diff.Item2) {
+			assert.Equal(t, diffPath("c2/s5/s3/0.bin"), diff.Item2.FullPath)
+			assert.True(t, diff.Item2.IsDir())
+		}
 	}
 
 	{
 		diff := diffs[13]
 		assert.Nil(t, diff.Item1)
-		assert.Equal(t, diffPath("c2/s5/zz"), diff.Item2.FullPath)
+		if assert.NotNil(t, diff.Item2) {
+			assert.Equal(t, diffPath("c2/s5/zz"), diff.Item2.FullPath)
+		}
 	}
 
 	{
 		diff := diffs[14]
 		assert.Nil(t, diff.Item1)
-		assert.Equal(t, diffPath("c2/s6"), diff.Item2.FullPath)
+		if assert.NotNil(t, diff.Item2) {
+			assert.Equal(t, diffPath("c2/s6"), diff.Item2.FullPath)
+		}
 	}
 }
 
