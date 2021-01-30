924c285919727f8a4a33b80461b4a76afd9b5d07
SladeThe <sladethe@gmail.com>
2021-01-30 17:01:23 +0400

    Minor test improvement

--- strings/sort_test.go
+++ strings/sort_test.go
@@ -8,267 +8,267 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
-type sortArgs struct {
+type numericLessArgs struct {
 	a string
 	b string
 }
 
-type sortWant struct {
+type numericLessWant struct {
 	less bool
 }
 
-var sortTests = []struct {
+var numericLessTests = []struct {
 	name string
-	args sortArgs
-	want sortWant
+	args numericLessArgs
+	want numericLessWant
 }{{
 	name: "empty",
-	args: sortArgs{},
-	want: sortWant{},
+	args: numericLessArgs{},
+	want: numericLessWant{},
 }, {
 	name: "empty - non empty",
-	args: sortArgs{b: "b"},
-	want: sortWant{less: true},
+	args: numericLessArgs{b: "b"},
+	want: numericLessWant{less: true},
 }, {
 	name: "non empty - empty",
-	args: sortArgs{a: "a"},
-	want: sortWant{less: false},
+	args: numericLessArgs{a: "a"},
+	want: numericLessWant{less: false},
 }, {
 	name: "alfa less",
-	args: sortArgs{a: "a", b: "b"},
-	want: sortWant{less: true},
+	args: numericLessArgs{a: "a", b: "b"},
+	want: numericLessWant{less: true},
 }, {
 	name: "alfa equal",
-	args: sortArgs{a: "v", b: "v"},
-	want: sortWant{less: false},
+	args: numericLessArgs{a: "v", b: "v"},
+	want: numericLessWant{less: false},
 }, {
 	name: "alfa more",
-	args: sortArgs{a: "b", b: "a"},
-	want: sortWant{less: false},
+	args: numericLessArgs{a: "b", b: "a"},
+	want: numericLessWant{less: false},
 }, {
 	name: "digit - non digit 1",
-	args: sortArgs{a: "1", b: "b"},
-	want: sortWant{less: true},
+	args: numericLessArgs{a: "1", b: "b"},
+	want: numericLessWant{less: true},
 }, {
 	name: "digit - non digit 2",
-	args: sortArgs{
+	args: numericLessArgs{
 		a: "T2 x",
 		b: "T  x",
 	},
-	want: sortWant{less: false},
+	want: numericLessWant{less: false},
 }, {
 	name: "non digit - digit 1",
-	args: sortArgs{a: "a", b: "1"},
-	want: sortWant{less: false},
+	args: numericLessArgs{a: "a", b: "1"},
+	want: numericLessWant{less: false},
 }, {
 	name: "non digit - digit 2",
-	args: sortArgs{
+	args: numericLessArgs{
 		a: "T  x",
 		b: "T2 x",
 	},
-	want: sortWant{less: true},
+	want: numericLessWant{less: true},
 }, {
 	name: "numbers 1",
-	args: sortArgs{a: "v123", b: "v9"},
-	want: sortWant{less: false},
+	args: numericLessArgs{a: "v123", b: "v9"},
+	want: numericLessWant{less: false},
 }, {
 	name: "numbers 2",
-	args: sortArgs{a: "8", b: "11"},
-	want: sortWant{less: true},
+	args: numericLessArgs{a: "8", b: "11"},
+	want: numericLessWant{less: true},
 }, {
 	name: "numbers 3",
-	args: sortArgs{a: "11", b: "11"},
-	want: sortWant{less: false},
+	args: numericLessArgs{a: "11", b: "11"},
+	want: numericLessWant{less: false},
 }, {
 	name: "zero numbers 1",
-	args: sortArgs{a: "01", b: "01"},
-	want: sortWant{less: false},
+	args: numericLessArgs{a: "01", b: "01"},
+	want: numericLessWant{less: false},
 }, {
 	name: "zero numbers 2",
-	args: sortArgs{a: "01", b: "1"},
-	want: sortWant{less: false},
+	args: numericLessArgs{a: "01", b: "1"},
+	want: numericLessWant{less: false},
 }, {
 	name: "zero numbers 3",
-	args: sortArgs{a: "1", b: "01"},
-	want: sortWant{less: true},
+	args: numericLessArgs{a: "1", b: "01"},
+	want: numericLessWant{less: true},
 }, {
 	name: "zero numbers 1a",
-	args: sortArgs{a: "v01v", b: "v01v"},
-	want: sortWant{less: false},
+	args: numericLessArgs{a: "v01v", b: "v01v"},
+	want: numericLessWant{less: false},
 }, {
 	name: "zero numbers 2a",
-	args: sortArgs{a: "v01v", b: "v1v"},
-	want: sortWant{less: false},
+	args: numericLessArgs{a: "v01v", b: "v1v"},
+	want: numericLessWant{less: false},
 }, {
 	name: "zero numbers 3a",
-	args: sortArgs{a: "v1v", b: "v01v"},
-	want: sortWant{less: true},
+	args: numericLessArgs{a: "v1v", b: "v01v"},
+	want: numericLessWant{less: true},
 }, {
 	name: "zero - non zero",
-	args: sortArgs{a: "v01v", b: "v2v"},
-	want: sortWant{less: true},
+	args: numericLessArgs{a: "v01v", b: "v2v"},
+	want: numericLessWant{less: true},
 }, {
 	name: "non zero - zero",
-	args: sortArgs{a: "v1v", b: "v02v"},
-	want: sortWant{less: true},
+	args: numericLessArgs{a: "v1v", b: "v02v"},
+	want: numericLessWant{less: true},
 }, {
 	name: "zero a",
-	args: sortArgs{a: "0", b: ""},
-	want: sortWant{less: false},
+	args: numericLessArgs{a: "0", b: ""},
+	want: numericLessWant{less: false},
 }, {
 	name: "zero b",
-	args: sortArgs{a: "", b: "0"},
-	want: sortWant{less: true},
+	args: numericLessArgs{a: "", b: "0"},
+	want: numericLessWant{less: true},
 }, {
 	name: "zeroes a",
-	args: sortArgs{a: "00", b: "0"},
-	want: sortWant{less: false},
+	args: numericLessArgs{a: "00", b: "0"},
+	want: numericLessWant{less: false},
 }, {
 	name: "zeroes b",
-	args: sortArgs{a: "0", b: "00"},
-	want: sortWant{less: true},
+	args: numericLessArgs{a: "0", b: "00"},
+	want: numericLessWant{less: true},
 }, {
 	name: "zeroes",
-	args: sortArgs{a: "000", b: "000"},
-	want: sortWant{less: false},
+	args: numericLessArgs{a: "000", b: "000"},
+	want: numericLessWant{less: false},
 }, {
 	name: "zeroes and letter a",
-	args: sortArgs{a: "000a", b: "000"},
-	want: sortWant{less: false},
+	args: numericLessArgs{a: "000a", b: "000"},
+	want: numericLessWant{less: false},
 }, {
 	name: "zeroes and letter b",
-	args: sortArgs{a: "000", b: "000b"},
-	want: sortWant{less: true},
+	args: numericLessArgs{a: "000", b: "000b"},
+	want: numericLessWant{less: true},
 }, {
 	name: "zeroes and letters 1",
-	args: sortArgs{a: "000a", b: "000b"},
-	want: sortWant{less: true},
+	args: numericLessArgs{a: "000a", b: "000b"},
+	want: numericLessWant{less: true},
 }, {
 	name: "zeroes and letters 2",
-	args: sortArgs{a: "00v", b: "000v"},
-	want: sortWant{less: true},
+	args: numericLessArgs{a: "00v", b: "000v"},
+	want: numericLessWant{less: true},
 }, {
 	name: "zeroes and letters 3",
-	args: sortArgs{a: "000v", b: "00v"},
-	want: sortWant{less: false},
+	args: numericLessArgs{a: "000v", b: "00v"},
+	want: numericLessWant{less: false},
 }, {
 	name: "zeroes digit - zeroes letter",
-	args: sortArgs{a: "0001", b: "000b"},
-	want: sortWant{less: false},
+	args: numericLessArgs{a: "0001", b: "000b"},
+	want: numericLessWant{less: false},
 }, {
 	name: "zeroes letter - zeroes digit",
-	args: sortArgs{a: "000a", b: "0001"},
-	want: sortWant{less: true},
+	args: numericLessArgs{a: "000a", b: "0001"},
+	want: numericLessWant{less: true},
 }, {
 	name: "numbers and letter 1",
-	args: sortArgs{a: "001v", b: "002v"},
-	want: sortWant{less: true},
+	args: numericLessArgs{a: "001v", b: "002v"},
+	want: numericLessWant{less: true},
 }, {
 	name: "numbers and letter 2",
-	args: sortArgs{a: "001v", b: "001v"},
-	want: sortWant{less: false},
+	args: numericLessArgs{a: "001v", b: "001v"},
+	want: numericLessWant{less: false},
 }, {
 	name: "numbers and letter 3",
-	args: sortArgs{a: "002v", b: "001v"},
-	want: sortWant{less: false},
+	args: numericLessArgs{a: "002v", b: "001v"},
+	want: numericLessWant{less: false},
 }, {
 	name: "numbers and letter 4",
-	args: sortArgs{a: "001a", b: "001b"},
-	want: sortWant{less: true},
+	args: numericLessArgs{a: "001a", b: "001b"},
+	want: numericLessWant{less: true},
 }, {
 	name: "numbers and letter 5",
-	args: sortArgs{a: "001b", b: "001a"},
-	want: sortWant{less: false},
+	args: numericLessArgs{a: "001b", b: "001a"},
+	want: numericLessWant{less: false},
 }, {
 	name: "numbers and letter 6",
-	args: sortArgs{a: "001", b: "001b"},
-	want: sortWant{less: true},
+	args: numericLessArgs{a: "001", b: "001b"},
+	want: numericLessWant{less: true},
 }, {
 	name: "numbers and letter 7",
-	args: sortArgs{a: "001a", b: "001"},
-	want: sortWant{less: false},
+	args: numericLessArgs{a: "001a", b: "001"},
+	want: numericLessWant{less: false},
 }, {
 	name: "long numbers 1",
-	args: sortArgs{
+	args: numericLessArgs{
 		a: strings.Repeat("0", 1000) + strings.Repeat("1", 1000) + "999",
 		b: strings.Repeat("0", 1000) + strings.Repeat("1", 1000) + "999",
 	},
-	want: sortWant{less: false},
+	want: numericLessWant{less: false},
 }, {
 	name: "long numbers 2",
-	args: sortArgs{
+	args: numericLessArgs{
 		a: strings.Repeat("0", 1000) + strings.Repeat("1", 1000) + "999",
 		b: strings.Repeat("0", 1000) + strings.Repeat("1", 1000) + "888",
 	},
-	want: sortWant{less: false},
+	want: numericLessWant{less: false},
 }, {
 	name: "long numbers 3",
-	args: sortArgs{
+	args: numericLessArgs{
 		a: strings.Repeat("0", 1000) + strings.Repeat("1", 1000) + "888",
 		b: strings.Repeat("0", 1000) + strings.Repeat("1", 1000) + "999",
 	},
-	want: sortWant{less: true},
+	want: numericLessWant{less: true},
 }, {
 	name: "long numbers 1a",
-	args: sortArgs{
+	args: numericLessArgs{
 		a: strings.Repeat("1", 1000) + "88",
 		b: strings.Repeat("1", 1000) + "999",
 	},
-	want: sortWant{less: true},
+	want: numericLessWant{less: true},
 }, {
 	name: "long numbers 2a",
-	args: sortArgs{
+	args: numericLessArgs{
 		a: strings.Repeat("1", 1000) + "99",
 		b: strings.Repeat("1", 1000) + "888",
 	},
-	want: sortWant{less: true},
+	want: numericLessWant{less: true},
 }, {
 	name: "long numbers 3a",
-	args: sortArgs{
+	args: numericLessArgs{
 		a: strings.Repeat("1", 1000) + "888",
 		b: strings.Repeat("1", 1000) + "99",
 	},
-	want: sortWant{less: false},
+	want: numericLessWant{less: false},
 }, {
 	name: "number groups 1",
-	args: sortArgs{
+	args: numericLessArgs{
 		a: "2021.1.0",
 		b: "2020.8.8",
 	},
-	want: sortWant{less: false},
+	want: numericLessWant{less: false},
 }, {
 	name: "number groups 2",
-	args: sortArgs{
+	args: numericLessArgs{
 		a: "2021.1.1",
 		b: "2021.1.2",
 	},
-	want: sortWant{less: true},
+	want: numericLessWant{less: true},
 }, {
 	name: "number groups 3",
-	args: sortArgs{
+	args: numericLessArgs{
 		a: "2021.1.0",
 		b: "2021.1.1",
 	},
-	want: sortWant{less: true},
+	want: numericLessWant{less: true},
 }, {
 	name: "number groups 4",
-	args: sortArgs{
+	args: numericLessArgs{
 		a: "2020.12.1",
 		b: "2020.12.1",
 	},
-	want: sortWant{less: false},
+	want: numericLessWant{less: false},
 }, {
 	name: "custom test 1",
-	args: sortArgs{
+	args: numericLessArgs{
 		a: "Terminator 2",
 		b: "Terminator Salvation",
 	},
-	want: sortWant{less: true},
+	want: numericLessWant{less: true},
 }}
 
 func BenchmarkNumericLess(b *testing.B) {
 	for i := 0; i < b.N; i++ {
-		for _, tt := range sortTests {
+		for _, tt := range numericLessTests {
 			if NumericLess(tt.args.a, tt.args.b) != tt.want.less {
 				require.Equal(b, tt.want.less, NumericLess(tt.args.a, tt.args.b), "invalid less")
 			}
@@ -277,13 +277,13 @@ func BenchmarkNumericLess(b *testing.B) {
 }
 
 func TestNumericLess(t *testing.T) {
-	test := func(a sortArgs, w sortWant) func(t *testing.T) {
+	test := func(a numericLessArgs, w numericLessWant) func(t *testing.T) {
 		return func(t *testing.T) {
 			assert.Equal(t, w.less, NumericLess(a.a, a.b), "invalid less")
 		}
 	}
 
-	for _, tt := range sortTests {
+	for _, tt := range numericLessTests {
 		t.Run(tt.name, test(tt.args, tt.want))
 	}
 }
